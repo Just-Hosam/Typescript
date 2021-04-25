988f95fb526cae89e41c550fc3359fe6e4ad673f
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-25 10:50:29 -0600

    ts types and enum example

---
+++ ts-hello/main.ts
@@ -0,0 +1,18 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[];
+let f: any[];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color {
+	Red = 0,
+	Green = 1,
+	Blue = 2,
+}
+
+let backgroundColor = Color.Red;
