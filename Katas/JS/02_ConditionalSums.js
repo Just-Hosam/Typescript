387a345938d14254c1980dbb7562bef979d176ff
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-28 20:15:16 -0600

    Conditional Sums

---
+++ Katas/JS/02_ConditionalSums.js
@@ -0,0 +1,16 @@
+"use strict";
+// For this kata, we'll be adding only the numbers in the array which match the given condition.
+const conditionalSum = (arr, condition) => {
+    let sum = 0;
+    for (const elem of arr) {
+        if (condition === 'even' && elem % 2 === 0)
+            sum += elem;
+        if (condition === 'odd' && elem % 2 !== 0)
+            sum += elem;
+    }
+    return sum;
+};
+console.log(conditionalSum([1, 2, 3, 4, 5], 'even'), '=== 6');
+console.log(conditionalSum([1, 2, 3, 4, 5], 'odd'), '=== 9');
+console.log(conditionalSum([13, 88, 12, 44, 99], 'even'), '=== 144');
+console.log(conditionalSum([], 'odd'), '=== 0');
