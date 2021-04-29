8f9020419696d705899843c9784c6315fbfd0e74
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-28 21:35:51 -0600

    Percent Encoded String

---
+++ Katas/TS/05_PercentEncodedString.ts
@@ -0,0 +1,14 @@
+// In this exercise, we will be given a normal string of words and turn it into a percent-encoded string by replacing all whitespace with %20.
+
+const urlEncode = (str: string): string => {
+	const trimmedStr = str.trim();
+
+	return trimmedStr.replace(' ', '%20');
+};
+
+console.log(urlEncode('Lighthouse Labs'), '=== Lighthouse%20Labs');
+console.log(urlEncode(' Lighthouse Labs '), '=== Lighthouse%20Labs');
+console.log(
+	urlEncode('blue is greener than purple for sure'),
+	'=== blue%20is%20greener%20than%20purple%20for%20sure'
+);
