523b7e1842e62f72d04334c1e19d412c09f3906e
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-29 23:14:32 -0600

    In the air tonight

---
+++ Katas/JS/07_InTheAirTonight.js
@@ -0,0 +1,23 @@
+"use strict";
+// The new smart parking lot in Codeville was a big hit! Next on your list to tackle is the air quality. You've decided that you want to install air pollution sensors around the city to monitor air quality and identify problem areas. We need to write the code for the sensors to trigger a special message when the air is too polluted.
+const checkAir = (samplesArr, threshold) => {
+    let dirtyCount = 0;
+    samplesArr.forEach((elem) => (elem === 'dirty' ? dirtyCount++ : null));
+    const pollutionRatio = dirtyCount / samplesArr.length;
+    const isPolluted = pollutionRatio > threshold;
+    return isPolluted ? 'Polluted' : 'Clean';
+};
+console.log(checkAir([
+    'clean',
+    'clean',
+    'dirty',
+    'clean',
+    'dirty',
+    'clean',
+    'clean',
+    'dirty',
+    'clean',
+    'dirty',
+], 0.3), '=== Polluted');
+console.log(checkAir(['dirty', 'dirty', 'dirty', 'dirty', 'clean'], 0.25), '=== Polluted');
+console.log(checkAir(['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'], 0.9), '=== Clean');
